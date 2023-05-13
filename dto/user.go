c81de5ffbd3e275b59371c54517a55aa060e0a5f
tfkhdyt <tfkhdyt@proton.me>
2023-05-13 14:53:27 +0700

    feat(user): add "update user" feature

--- dto/user.go
+++ dto/user.go
@@ -29,6 +29,8 @@ type RegisterResponse struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+// ==========================================
+
 type LoginRequest struct {
 	Email    string `json:"email" binding:"email,required"`
 	Password string `json:"password" binding:"required,min=6"`
@@ -36,4 +38,25 @@ type LoginRequest struct {
 
 type LoginResponse struct {
 	Token string `json:"token" binding:"jwt"`
+}
+
+// =============================================
+
+type UpdateUserRequest struct {
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"email,required"`
+}
+
+func (r *UpdateUserRequest) ToEntity() *entity.User {
+	return &entity.User{
+		FullName: r.FullName,
+		Email:    r.Email,
+	}
+}
+
+type UpdateUserResponse struct {
+	ID        uint      `json:"id"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- handler/http_handler/user_handler.go
+++ handler/http_handler/user_handler.go
@@ -2,6 +2,7 @@ package http_handler
 
 import (
 	"hacktiv8-msib-final-project-3/dto"
+	"hacktiv8-msib-final-project-3/entity"
 	"hacktiv8-msib-final-project-3/pkg/errs"
 	"hacktiv8-msib-final-project-3/service"
 	"net/http"
@@ -53,4 +54,28 @@ func (u *userHandler) Login(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, token)
+}
+
+func (u *userHandler) UpdateUser(ctx *gin.Context) {
+	var requestBody dto.UpdateUserRequest
+	userData, ok := ctx.MustGet("userData").(*entity.User)
+	if !ok {
+		newError := errs.NewBadRequest("Failed to get user data")
+		ctx.JSON(newError.StatusCode(), newError)
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
+		newError := errs.NewUnprocessableEntity(err.Error())
+		ctx.JSON(newError.StatusCode(), newError)
+		return
+	}
+
+	updatedUser, err := u.userService.UpdateUser(userData, &requestBody)
+	if err != nil {
+		ctx.JSON(err.StatusCode(), err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, updatedUser)
 }
