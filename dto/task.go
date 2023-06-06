5a28906b3c01cd293fdd197995dcbef2bee4774d
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-06 15:54:38 +0800

    fix: response GET /tasks

--- dto/task.go
+++ dto/task.go
@@ -48,7 +48,7 @@ type GetAllTasksResponse struct {
 	UserID      uint      `json:"user_id"`
 	CategoryID  uint      `json:"category_id"`
 	CreatedAt   time.Time `json:"created_at"`
-	User        UserData  `json:"user"`
+	User        UserData  `json:"User"`
 }
 
 type UpdateTaskRequest struct {
